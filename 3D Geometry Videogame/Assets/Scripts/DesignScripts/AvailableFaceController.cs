2033ceb20cfb6c1631c79e45d5af0958fe1d6afb
Oriol Sors <oriolsors@gmail.com>
2021-10-18 22:11:58 +0200

    New rotation method and repositioning zoom camera
    RotateAround function used with InputAxis as degree parameter. Mouse Scrollwheel used as camera zoom. Bounds and object VRP are recalculated when Instantiate.

--- 3D Geometry Videogame/Assets/Scripts/DesignScripts/AvailableFaceController.cs
+++ 3D Geometry Videogame/Assets/Scripts/DesignScripts/AvailableFaceController.cs
@@ -6,133 +6,95 @@ using UnityEngine;
 public class AvailableFaceController : MonoBehaviour
 {
     public GameObject cubePrefab;
-    private Vector3 cameraDir;
+    public GameObject boundaryBox;
     private CameraController scriptCamera;
 
-    private Vector3 currentVRP;
 
     // Start is called before the first frame update
     void Start()
     {
         scriptCamera = GameObject.Find("Main Camera").GetComponent<CameraController>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        cameraDir = scriptCamera.cameraDir;
+
     }
 
     private void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject newCube;  //TODO: Evitar inserir un cub a sobre un altre
+            GameObject newCube;  //TODO: Evitar inserir un cub al mateix lloc que un altre
 
 
-            if (-cameraDir == transform.up)
+            // RAY CASTING TO FIND THE AVAILABLE FACE TO ADD THE NEW OBJECT
+
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
             {
-                newCube = Instantiate(cubePrefab, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Quaternion.identity) as GameObject;
-
-            }
-            else if (-cameraDir == -transform.up)
-            {
-                newCube = Instantiate(cubePrefab, new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), Quaternion.identity) as GameObject;
-
-            }
-            else if (-cameraDir == transform.right)
-            {
-                newCube = Instantiate(cubePrefab, new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
-
-            }
-            else if (-cameraDir == -transform.right)
-            {
-                newCube = Instantiate(cubePrefab, new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
-
-            }
-            else if (-cameraDir == transform.forward)
-            {
-                newCube = Instantiate(cubePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.5f), Quaternion.identity) as GameObject;
-
-            }
-            else //if (-cameraDir == -transform.forward)
-            {
-                newCube = Instantiate(cubePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.5f), Quaternion.identity) as GameObject;
-
+                if (hit.collider != null)
+                {
+                    newCube = Instantiate(cubePrefab, hit.transform.position + 0.5f*hit.normal, Quaternion.identity) as GameObject; //TODO: crear metodes alternatius per a la resta de solids platonics -> segons l'escala de cadascun
+                    newCube.transform.parent = boundaryBox.transform;
+                    Bounds bounds = GetBounds(boundaryBox);
+                    scriptCamera.currentVRP = bounds.center;
+                    scriptCamera.boxBounds = bounds;
+                    
+                }
             }
 
-            currentVRP = newCube.transform.position;
 
-            UpdateVRP(currentVRP);
-
-            //Vector3 translation = NewCameraPosition(scriptCamera, newCube);
-            if (Vector3.Distance(scriptCamera.transform.position, currentVRP) <= 2.5)
-            {
-                scriptCamera.transform.Translate(-cameraDir * 0.5f, Space.World); //TODO: actualitzar acuradament la posicio de la camera ??
-            }
         }
 
         if (Input.GetMouseButtonDown(1))
         {
             Destroy(gameObject);
+            Bounds bounds = GetBounds(boundaryBox);
+            scriptCamera.currentVRP = bounds.center;
+            scriptCamera.boxBounds = bounds;
+            
         }
         
-            
-
 
     }
-
-
-    private void UpdateVRP(Vector3 currentVRP)
+    private Bounds GetBounds(GameObject boundaryBox)
     {
-        Vector3 upVRP = scriptCamera.upVRP;
-        Vector3 downVRP = scriptCamera.downVRP;
-        Vector3 rightVRP = scriptCamera.rightVRP;
-        Vector3 leftVRP = scriptCamera.leftVRP;
-        Vector3 forwardVRP = scriptCamera.forwardVRP;
-        Vector3 backVRP = scriptCamera.backVRP;
-
-        if (currentVRP.y > upVRP.y)
+        Bounds bounds;
+        Renderer childRender;
+        bounds = GetRenderBounds(boundaryBox);
+        if (bounds.extents.x == 0)
         {
-            scriptCamera.upVRP = currentVRP;
-        }
-        if (currentVRP.y < downVRP.y)
-        {
-            scriptCamera.downVRP = currentVRP;
-        }
-        if (currentVRP.x > rightVRP.x)
-        {
-            scriptCamera.rightVRP = currentVRP;
-        }
-        if (currentVRP.x < leftVRP.x)
-        {
-            scriptCamera.leftVRP = currentVRP;
-        }
-        if (currentVRP.z > forwardVRP.z)
-        {
-            scriptCamera.forwardVRP = currentVRP;
-        }
-        if (currentVRP.z < backVRP.z)
-        {
-            scriptCamera.backVRP = currentVRP;
-        }
-    }
-
-    private Vector3 NewCameraPosition(CameraController scriptCamera, GameObject newCube)
-    {
-        Vector3 cameraPos = scriptCamera.transform.localPosition;
-        Vector3 cubePos = newCube.transform.localPosition;
-        if (Vector3.Distance(cameraPos,cubePos) < 3)
-        {
-            if (cameraPos.z - cubePos.z > 0)
+            bounds = new Bounds(boundaryBox.transform.position, Vector3.zero);
+            foreach (Transform child in boundaryBox.transform)
             {
-                return new Vector3(0, 0, 3 + cubePos.z - cameraPos.z);
-            }else if (cameraPos.z - cubePos.z < 0)
-            {
-                return new Vector3(0, 0, -3 + cubePos.z - cameraPos.z);
+                childRender = child.GetComponent<Renderer>();
+                if (childRender)
+                {
+                    bounds.Encapsulate(childRender.bounds);
+                }
+                else
+                {
+                    bounds.Encapsulate(GetBounds(child.gameObject));
+                }
             }
         }
-        return Vector3.zero;
+        return bounds;
     }
+
+    private Bounds GetRenderBounds(GameObject boundaryBox)
+    {
+        Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
+        Renderer render = boundaryBox.GetComponent<Renderer>();
+        if (render != null)
+        {
+            return render.bounds;
+        }
+        return bounds;
+    }
+
+
 }
--- 3D Geometry Videogame/Assets/Scripts/DesignScripts/CameraController.cs
+++ 3D Geometry Videogame/Assets/Scripts/DesignScripts/CameraController.cs
@@ -8,253 +8,72 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private float speed;
 
-    [SerializeField]
-    public Vector3 cameraDir { get; private set; }
-
     public Vector3 currentVRP;
 
-    public Vector3 upVRP;
-    public Vector3 downVRP;
-    public Vector3 rightVRP;
-    public Vector3 leftVRP;
-    public Vector3 forwardVRP;
-    public Vector3 backVRP;
+    public Bounds boxBounds;
 
 
-
-    private Vector3 directionToFace;
-    private Vector3 initialDirection;
-
-    private float rotationAngle = 0f;
-
-    private bool activeUp = false;
-    private bool activeDown = false;
-    private bool activeRight = false;
-    private bool activeLeft = false;
+    Vector3 cameraPos = new Vector3(0, 0, 0);
+    Vector3 closestPoint = new Vector3(0, 0, 0);
 
     void Start()
     {
-        cameraDir = transform.forward;
-        currentVRP = new Vector3(0, 0, 0); //TODO: llista amb els valors mes alts a UP, -UP, RIGHT, -RIGHT, FORWARD, -FORWARD i en els m?todes de Rotate seleccionar el VRP m?s adequat
-        upVRP = new Vector3(0, 0, 0);
-        downVRP = new Vector3(0, 0, 0);
-        rightVRP = new Vector3(0, 0, 0);
-        leftVRP = new Vector3(0, 0, 0);
-        forwardVRP = new Vector3(0, 0, 0);
-        backVRP = new Vector3(0, 0, 0);
+        currentVRP = new Vector3(0, 0, 0);
+        boxBounds = new Bounds();
+
     }
 
     private void Update()
     {
-        
+        RecalculatePos();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
+        Debug.DrawRay(cameraPos, closestPoint - cameraPos, Color.green);
+        Debug.DrawRay(cameraPos, currentVRP - cameraPos, Color.blue);
+
+        
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(transform.right * speed * horizontalInput * Time.fixedDeltaTime, Space.World);
-        transform.Translate(transform.up * speed * verticalInput * Time.fixedDeltaTime, Space.World);
 
-        if (activeUp)
-        {
-            directionToFace = currentVRP - transform.position;
-            rotationAngle = Vector3.Angle(initialDirection, directionToFace);
-            
-            RotateUp();
-        }
-        if (activeDown)
-        {
-            directionToFace = currentVRP - transform.position;
-            rotationAngle = Vector3.Angle(initialDirection, directionToFace);
-            
-            RotateDown();
-        }
-        if (activeRight)
-        {
-            directionToFace = currentVRP - transform.position;
-            rotationAngle = Vector3.Angle(initialDirection, directionToFace);
-            
-            RotateRight();
-        }
-        if (activeLeft)
-        {
-            directionToFace = currentVRP - transform.position;
-            rotationAngle = Vector3.Angle(initialDirection, directionToFace);
-            
-            RotateLeft();
-        }
-
+        transform.RotateAround(currentVRP, transform.up, -horizontalInput * Time.fixedDeltaTime * speed);
+        transform.RotateAround(currentVRP, transform.right, verticalInput * Time.fixedDeltaTime * speed);
     }
 
-    public void RotateUp()
+    public void RecalculatePos()
     {
-        /*
-        Vector3 crossProduct = Vector3.Cross(directionToFace, Vector3.up);
-
-        if (Vector3.Distance(crossProduct, Vector3.zero) < 0.1f && directionToFace.normalized == -Vector3.up)
+        cameraPos = transform.position;
+        Vector3 repositionDir;
+        Vector3 repositionDirNorm;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
-            activeUp = false;
+            repositionDir = currentVRP - cameraPos;
+            repositionDirNorm = Vector3.Normalize(repositionDir);
+            transform.Translate(repositionDirNorm * 0.5f, Space.World);
+        } 
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            repositionDir = -currentVRP + cameraPos;
+            repositionDirNorm = Vector3.Normalize(repositionDir);
+            transform.Translate(repositionDirNorm * 0.5f, Space.World);
         }
+            
 
-        Quaternion rotation = Quaternion.LookRotation(directionToFace);
-
-        Quaternion current = transform.rotation;
-
-        transform.rotation = Quaternion.Slerp(current, rotation, 50 * Time.deltaTime);
-
-        transform.Translate(0, 5 * Time.deltaTime, 0);
-
+        /*
+        closestPoint = boxBounds.ClosestPoint(cameraPos); //TODO: Al ser un Boundary Box, per geometria sempre obtindrem un punt de la cara de la Box
+        Vector3 repositionDir = closestPoint - cameraPos;
+        Vector3 repositionDirNorm = Vector3.Normalize(repositionDir);
+        transform.Translate(repositionDir, Space.World);
+        transform.Translate(repositionDirNorm*0.5f, Space.World);
         */
 
-        if(rotationAngle >= 90)
-        {
-            activeUp = false;
-            
-        }
-        else
-        {
-            transform.RotateAround(currentVRP, transform.right, 90 * Time.deltaTime);
-
-            cameraDir = Vector3Int.RoundToInt(transform.forward);
-        }
-
+        
+        
         
 
         
-
     }
-
-
-    public void RotateDown()
-    {
-
-        if (rotationAngle >= 90)
-        {
-            activeDown = false;
-            
-        }
-        else
-        {
-            transform.RotateAround(currentVRP, -transform.right, 90 * Time.deltaTime);
-
-            cameraDir = Vector3Int.RoundToInt(transform.forward);
-        }
-
-        
-
-    }
-
-    public void RotateRight()
-    {
-        /*
-        Vector3 crossProduct = Vector3.Cross(directionToFace, Vector3.right);
-
-        if (Vector3.Distance(crossProduct, Vector3.zero) < 0.1f && directionToFace.normalized == -Vector3.right)
-        {
-            activeRight = false;
-        }
-
-
-        Quaternion rotation = Quaternion.LookRotation(directionToFace);
-
-        Quaternion current = transform.rotation;
-
-        transform.rotation = Quaternion.Slerp(current, rotation, 50*Time.deltaTime);
-
-        transform.Translate(5*Time.deltaTime, 0, 0);
-
-        */
-
-        if (rotationAngle >= 90)
-        {
-            activeRight = false;
-            
-        }
-        else
-        {
-            transform.RotateAround(currentVRP, -transform.up, 90 * Time.deltaTime);
-
-            cameraDir = Vector3Int.RoundToInt(transform.forward);
-        }
-
-        
-
-    }
-
-    public void RotateLeft()
-    {
-        if (rotationAngle >= 90)
-        {
-            activeLeft = false;
-            
-        }
-        else
-        {
-            transform.RotateAround(currentVRP, transform.up, 90 * Time.deltaTime);
-
-            cameraDir = Vector3Int.RoundToInt(transform.forward);
-        }
-
-        
-    }
-
-
-
-    private Vector3 GetCorrectVRP(Vector3 dir)
-    {
-        if (dir == Vector3.up)
-        {
-            currentVRP = upVRP;
-        }
-        else if (dir == -Vector3.up)
-        {
-            currentVRP = downVRP;
-        }
-        else if (dir == Vector3.forward)
-        {
-            currentVRP = forwardVRP;
-        }
-        else if (dir == -Vector3.forward)
-        {
-            currentVRP = backVRP;
-        }
-        else if (dir == Vector3.right)
-        {
-            currentVRP = rightVRP;
-        }
-        else //if (dir == -Vector3.right)
-        {
-            currentVRP = leftVRP;
-        }
-
-        return currentVRP;
-    }
-
-
-    public void ActiveRotateUp()
-    {
-        activeUp = true;
-        initialDirection = GetCorrectVRP(transform.up) - transform.position;
-    }
-
-    public void ActiveRotateDown()
-    {
-        activeDown = true;
-        initialDirection = GetCorrectVRP(-transform.up) - transform.position;
-    }
-
-    public void ActiveRotateRight()
-    {
-        activeRight = true;
-        initialDirection = GetCorrectVRP(transform.right) - transform.position;
-    }
-
-    public void ActiveRotateLeft()
-    {
-        activeLeft = true;
-        initialDirection = GetCorrectVRP(-transform.right) - transform.position;
-    }
-
 }
