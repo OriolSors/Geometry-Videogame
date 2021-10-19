952ba80b18fb4445ce93f20ab9eabba481715025
Oriol Sors <oriolsors@gmail.com>
2021-10-19 03:13:24 +0200

    Cleaning code from Available Face Controller

--- 3D Geometry Videogame/Assets/Scripts/DesignScripts/AvailableFaceController.cs
+++ 3D Geometry Videogame/Assets/Scripts/DesignScripts/AvailableFaceController.cs
@@ -5,53 +5,73 @@ using UnityEngine;
 
 public class AvailableFaceController : MonoBehaviour
 {
-    public GameObject cubePrefab;
-    public GameObject boundaryBox;
-    private CameraController scriptCamera;
-    private List<Vector3> cubePositions;
+
+    // ------------------------------------------------ DECLARATIONS ------------------------------------------------
 
 
-    // Start is called before the first frame update
+    public GameObject cubePrefab; //Cube prefab
+
+    public GameObject boundaryBox; //Parent Bounding Box that englobes all the construction
+
+    private CameraController scriptCamera; //The Main Camera script
+
+    private List<Vector3> cubePositions; //List of Cube prefab positions
+
+
+    // ------------------------------------------------ INITIALIZATIONS ------------------------------------------------
+
+
     void Start()
     {
+
         scriptCamera = GameObject.Find("Main Camera").GetComponent<CameraController>();
         cubePositions = new List<Vector3>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
 
     }
 
+
+    // ------------------------------------------------ MOUSE CLICK LISTENERS ------------------------------------------------
     private void OnMouseOver()
     {
+        //Left click on mouse
+
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject newCube;  //TODO: Evitar inserir un cub al mateix lloc que un altre
-
-
-            // RAY CASTING TO FIND THE AVAILABLE FACE TO ADD THE NEW OBJECT
+            
+            // Ray Casting to find the available face to add the new object
 
             RaycastHit hit;
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.collider != null)
+                if (hit.collider != null) 
                 {
-                    newCube = Instantiate(cubePrefab, hit.transform.position + 0.5f*hit.normal, Quaternion.identity) as GameObject; //TODO: crear metodes alternatius per a la resta de solids platonics -> segons l'escala de cadascun
-                    if (!cubePositions.Contains(newCube.transform.position))
+                    //Adds new Cube prefab to the normal direction of face clicked
+
+                    GameObject newCube = Instantiate(cubePrefab, hit.transform.position + 0.5f*hit.normal, Quaternion.identity) as GameObject; //TODO: for all the other Platonic Solids, create new methods to scale and position the object
+                    Vector3 newCubePosition = newCube.transform.position;
+
+                    if (!cubePositions.Contains(newCubePosition)) 
                     {
-                        cubePositions.Add(newCube.transform.position);
-                        newCube.transform.parent = boundaryBox.transform;
-                        Bounds bounds = GetBounds(boundaryBox);
-                        scriptCamera.currentVRP = bounds.center;
-                        scriptCamera.boxBounds = bounds;
+
+                        //If there is not an object at the new position, we add it
+
+                        cubePositions.Add(newCubePosition);
+                        newCube.transform.parent = boundaryBox.transform; 
+
+                        //Sending Bounding Box bounds and VRP to the Camera Controller
+
+                        SendBounds();
+
                     }
                     else
                     {
-                        Destroy(newCube);
+
+                        //If there is an object at the new position, we destroy it instantly
+
+                        Destroy(newCube); 
+
                     }
                     
                 }
@@ -59,16 +79,36 @@ public class AvailableFaceController : MonoBehaviour
 
         }
 
+        //Right click on mouse
+
         if (Input.GetMouseButtonDown(1))
         {
             Destroy(gameObject);
-            Bounds bounds = GetBounds(boundaryBox);
-            scriptCamera.currentVRP = bounds.center;
-            scriptCamera.boxBounds = bounds;
+
+            //Sending Bounding Box bounds and VRP to the Camera Controller+
+
+            SendBounds();
+
         }
         
 
     }
+
+
+    // ------------------------------------------------ BOUNDS SETTER ------------------------------------------------
+
+
+    private void SendBounds()
+    {
+        Bounds bounds = GetBounds(boundaryBox);
+        scriptCamera.currentVRP = bounds.center;
+        scriptCamera.boxBounds = bounds;
+    }
+
+
+    // ------------------------------------------------ BOUNDS GENERATORS ------------------------------------------------
+
+
     private Bounds GetBounds(GameObject boundaryBox)
     {
         Bounds bounds;
