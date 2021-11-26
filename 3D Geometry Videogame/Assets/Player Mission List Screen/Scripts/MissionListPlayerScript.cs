e3ddf9549c23eba622ab1b444225337bc56b859b
Oriol Sors <oriolsors@gmail.com>
2021-11-26 18:33:04 +0100

    TAB Inventory Canvas implemented at Player Mission List Screen.

--- 3D Geometry Videogame/Assets/Player Mission List Screen/Scripts/MissionListPlayerScript.cs
+++ 3D Geometry Videogame/Assets/Player Mission List Screen/Scripts/MissionListPlayerScript.cs
@@ -8,17 +8,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 public class MissionListPlayerScript : MonoBehaviour
 {
     private string username;
 
+    public RectTransform missionsScroll;
     public GameObject missionView;
     public GameObject missionReadyView;
 
-    private DatabaseReference reference;
+    public Canvas inventoryCanvas;
+    public TextMeshProUGUI inventoryMissionTitle;
+    public RectTransform inventoryMissionsScroll;
+    public GameObject inventoryMissionView;
+    public TextMeshProUGUI inventoryCubeNumbers;
+    public TextMeshProUGUI inventoryMissionStatus;
 
-    public RectTransform missionsScroll;
+
+    private DatabaseReference reference;
 
     public Font font;
 
@@ -27,6 +35,13 @@ public class MissionListPlayerScript : MonoBehaviour
         reference = FirebaseDatabase.GetInstance("https://geometry-videog-default-rtdb.firebaseio.com/").RootReference;
         LoadUser();
         LoadMissionsWithUser(FillMissionScroll);
+        inventoryCanvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Tab)) inventoryCanvas.enabled = true;
+        else inventoryCanvas.enabled = false;
     }
 
     private void FillMissionScroll(Dictionary<string, List<int>> missions)
@@ -37,6 +52,8 @@ public class MissionListPlayerScript : MonoBehaviour
             
             if (missions[mission][0] >= missions[mission][1])
             {
+                //---------- MAIN LIST MISSIONS ----------
+
                 GameObject go = Instantiate(missionReadyView);
                 Button minigameMission = go.transform.Find("Mission Button").GetComponent<Button>();
                 Button finishMission = go.transform.Find("Ready Button").GetComponent<Button>();
@@ -47,9 +64,22 @@ public class MissionListPlayerScript : MonoBehaviour
 
                 go.transform.SetParent(missionsScroll);
 
+                //---------- INVENTORY LIST MISSIONS ----------
+
+                GameObject inventoryGo = Instantiate(inventoryMissionView);
+                Button inventoryMission = inventoryGo.transform.Find("Mission Button").GetComponent<Button>();
+
+                inventoryMission.GetComponentInChildren<Text>().text = mission;
+                inventoryMission.onClick.AddListener(delegate { SetInventoryMission(mission, "DONE!", missions[mission][0].ToString()); });
+
+                inventoryGo.transform.SetParent(inventoryMissionsScroll);
+
+
             }
             else
             {
+                //---------- MAIN LIST MISSIONS ----------
+
                 GameObject go = Instantiate(missionView);
                 Button minigameMission = go.transform.Find("Mission Button").GetComponent<Button>();
 
@@ -58,13 +88,51 @@ public class MissionListPlayerScript : MonoBehaviour
                 minigameMission.onClick.AddListener(delegate { LoadMinigames(mission, missions[mission][0]); });
 
                 go.transform.SetParent(missionsScroll);
-            }
-            
 
-            
+                //---------- INVENTORY LIST MISSIONS ----------
+
+                GameObject inventoryGo = Instantiate(inventoryMissionView);
+                Button inventoryMission = inventoryGo.transform.Find("Mission Button").GetComponent<Button>();
+
+                inventoryMission.GetComponentInChildren<Text>().text = mission;
+                inventoryMission.onClick.AddListener(delegate { SetInventoryMission(mission, (100 * missions[mission][0] / missions[mission][1]).ToString() + "%", missions[mission][0].ToString()); });
+
+                inventoryGo.transform.SetParent(inventoryMissionsScroll);
+
+            }
 
         }
+        string firstMission = missions.Keys.First();
+        if (missions[firstMission] != null && missions[firstMission][0] >= missions[firstMission][1])
+        {
+            SetInventoryMission(firstMission, "DONE!", missions[firstMission][0].ToString());
+        }
+        else if (missions[firstMission] != null && missions[firstMission][0] < missions[firstMission][1]) 
+        { 
+            SetInventoryMission(firstMission, (100 * missions[firstMission][0] / missions[firstMission][1]).ToString() + "%", missions[firstMission][0].ToString()); 
+        }
+
+
     }
+
+    private void SetInventoryMission(string mission, string inventoryStatus, string inventoryNumber)
+    {
+        inventoryMissionTitle.text = mission;
+        inventoryMissionStatus.text = inventoryStatus;
+        inventoryCubeNumbers.text = inventoryNumber;
+
+        if (inventoryStatus == "DONE!")
+        {
+            inventoryMissionStatus.color = Color.yellow;
+            inventoryCubeNumbers.color = Color.yellow;
+        }
+        else
+        {
+            inventoryMissionStatus.color = Color.white;
+            inventoryCubeNumbers.color = Color.white;
+        }
+    }
+
     private void GoToConstruct()
     {
         SceneManager.LoadScene("3D Constructor");
