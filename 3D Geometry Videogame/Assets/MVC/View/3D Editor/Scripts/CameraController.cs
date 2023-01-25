9f49c4b2db8fd7524d217434a205c43ecb557899
Oriol Sors <oriolsors@gmail.com>
2023-01-25 01:30:50 +0100

    Change camera controls to Unity-like

--- 3D Geometry Videogame/Assets/MVC/View/3D Editor/Scripts/CameraController.cs
+++ 3D Geometry Videogame/Assets/MVC/View/3D Editor/Scripts/CameraController.cs
@@ -38,67 +38,65 @@ public class CameraController : MonoBehaviour
 
     // ------------------------------------------------ UPDATING METHODS PER FRAME ------------------------------------------------
 
-
     private void Update()
     {
-        //Recalculate the Main Camera position and local rotation at each frame
 
-        RecalculatePos(); 
+        //---- TRANSLATION ----
+        Vector3 translation = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) translation += transform.forward;
+        if (Input.GetKey(KeyCode.A)) translation -= transform.right;
+        if (Input.GetKey(KeyCode.S)) translation -= transform.forward;
+        if (Input.GetKey(KeyCode.D)) translation += transform.right;
+        if (Input.GetKey(KeyCode.Q)) translation += Vector3.up;
+        if (Input.GetKey(KeyCode.E)) translation -= Vector3.up;
 
-    }
+        transform.Translate(translation * Time.fixedDeltaTime * 0.80f, Space.World);
 
-    private void FixedUpdate()
-    {
-        //Smooth orbital rotation at 50 FPS
+        //---- ZOOM ----
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        transform.RotateAround(currentVRP, transform.up, -horizontalInput * Time.fixedDeltaTime * speed);
-        transform.RotateAround(currentVRP, transform.right, verticalInput * Time.fixedDeltaTime * speed);
-
-    }
-
-
-    // ------------------------------------------------ RECALCULATING THE MAIN CAMERA POSITION / ROTATION ------------------------------------------------
-
-
-    public void RecalculatePos()
-    {
-        //Gets the current Main Camera position and its closest point to the Bounding Box
-
-        cameraPos = transform.position; 
+        cameraPos = transform.position;
         closestPoint = boxBounds.ClosestPoint(cameraPos);
         float distance = Vector3.Distance(cameraPos, closestPoint);
-
-        //Gets the current Main Camera rotation and the target ones to the Bounding Box VRP
-
         Vector3 repositionDir = currentVRP - cameraPos;
-        Quaternion current = transform.rotation;
-        Quaternion target = Quaternion.LookRotation(repositionDir, transform.up);
-        
-        //Spherically interpolates both Quaternions in order to reorientate the Main Camera to the VRP
-
-        transform.localRotation = Quaternion.Slerp(current, target, Time.deltaTime);
-
-        if (distance < 1) transform.position = Vector3.Slerp(cameraPos, cameraPos - Vector3.Normalize(repositionDir) * 0.5f, Time.deltaTime * 5);
-
-        //Zoom IN
 
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
             if (distance >= 2) transform.Translate(Vector3.Normalize(repositionDir) * 0.5f, Space.World); //Translating the Main Camera to VRP with magnitude 0.5 
-        } 
-
-        //Zoom OUT
+        }
 
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
-            repositionDir = -repositionDir;
-            if (distance <= 5) transform.Translate(Vector3.Normalize(repositionDir) * 0.5f, Space.World); //Moving the Main Camera away from VRP with magnitude 0.5 
+            if (distance <= 5) transform.Translate(Vector3.Normalize(-repositionDir) * 0.5f, Space.World); //Moving the Main Camera away from VRP with magnitude 0.5 
         }
 
-        //Debugging tools
+        bool leftAltClicked = Input.GetKey(KeyCode.LeftAlt);
+        bool mouseLeftClicked = Input.GetMouseButton(0);
+        bool mouseRightClicked = Input.GetMouseButton(1);
+
+        //---- ROTATION ----
+        Vector3 rotation = Vector3.zero;
+        if (mouseRightClicked)
+        {
+            float horizontalInput = Input.GetAxis("Mouse X");
+            float verticalInput = Input.GetAxis("Mouse Y");
+
+            rotation.y = horizontalInput;
+            rotation.x = -verticalInput;
+
+            transform.Rotate(rotation * Time.fixedDeltaTime * speed);
+        }
+
+        //---- ORBIT ----
+        if (leftAltClicked && mouseLeftClicked)
+        {
+            float horizontalInput = Input.GetAxis("Mouse X");
+            float verticalInput = Input.GetAxis("Mouse Y");
+
+            transform.RotateAround(currentVRP, transform.up, horizontalInput * Time.fixedDeltaTime * speed);
+            transform.RotateAround(currentVRP, transform.right, -verticalInput * Time.fixedDeltaTime * speed);
+        }
+
+        //---- Debugging tools ----
         Debug.DrawRay(cameraPos, closestPoint - cameraPos, Color.green);
         Debug.DrawRay(cameraPos, currentVRP - cameraPos, Color.blue);
 
